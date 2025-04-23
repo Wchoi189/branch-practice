37f85d1d07e0806487a623e697d2bde7ccc77e88
wchoi189 <wchoi189@gmail.com>
2025-04-23 12:07:49 +0900

    feat: prints buzz when i is multiple of 5, prints fizzbuzz when i is multiple of 15

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,6 @@
-for i in range(1, 19+1):
+for i in range(1, 15+1):
     if i % 15 == 0:
         print('FizzBuzz')
-    elif i % 3 == 0:
-        print('Fizz')
     elif i % 5 == 0:
         print('Buzz')
     else:
