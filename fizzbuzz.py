427fb1db59ce7d05702f07619c4cb076dff5bc86
wchoi189 <wchoi189@gmail.com>
2025-04-23 10:51:36 +0900

    Fixed conflict between main and fizz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 for i in range(1,19+1):
     if i % 15 == 0:
         print('fizzbuzz')
     elif i % 5 == 0:
         print('fizz')    
-=======
-for i in range(1, 19+1):
-    if i % 15 == 0:
-        print('fizzbuzz')
-    elif i % 5 == 0:
->>>>>>> main
-        print('fizz')
     elif i % 5 == 0:
         print('buzz')
     else:
